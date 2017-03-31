7e848e0212fa4659e1391068e53f7437c8d83c8c
Jim Tyhurst <jim@tyhurst.com>
2017-03-30 23:50:43 -0700

    Migrated from deprecated 'pageWithSidebar' layout to 'fluidPage'.

--- server.R
+++ server.R
@@ -10,36 +10,32 @@ source("./data.R")
 # total budget.
 shinyServer(function(input, output) {
 
+  # Translates budgetLevel selection to displayable name.
+  budgetLevelNameMap <- list("service_area_code" = "Service Area", "bureau_code" = "Bureau", "object_code" = "Accounting Object")
+
   ####################
   # Generates a plot of the requested budgetLevel.
 
   # Computes the caption in a reactive expression, because
-  # it depends on the choices of budgetLevel and year.
+  # it depends on the choices of budgetLevel and fiscalYear.
   captionText <- reactive({
-    paste("By ", budgetLevelNameMap[input$budgetLevel], " for ", input$year)
+    paste("By", budgetLevelNameMap[input$budgetLevel], "for", input$fiscalYear)
   })
 
-  # Returns text for printing as the caption.
-  output$caption <- renderText({
-    captionText()
+  budgetLevel <- reactive({
+    budgetLevelNameMap[input$budgetLevel]
   })
 
   ####################
   # Generates a chart.
   # TODO: Plot should be responsive to choice of budgetLevel.
 
-  # Translates budgetLevel selection to displayable name.
-  budgetLevelNameMap <- list("service_area_code" = "Service Area", "bureau_code" = "Bureau", "object_code" = "Accounting Object")
-  budgetTitle <- "Budget for the City of Portland"
-  budgetXLabel <- reactive({budgetLevelNameMap[input$budgetLevel]})
-  budgetYLabel <- reactive({paste("Amount")})
-
   output$budgetPlot <- renderPlot({
-    ggplot(data=getServiceAreaTotals(input$year), aes(x=service_area_code, y=amount)) +
-      xlab("Service Area") +
+    ggplot(data=getServiceAreaTotals(input$fiscalYear), aes(x=service_area_code, y=amount)) +
+      xlab(budgetLevel()) +
       ylab("Amount") +
       geom_bar(stat="identity") +
-      ggtitle(budgetTitle)
+      ggtitle(paste("Budget for the City of Portland:", captionText()))
   })
 
 })
--- ui.R
+++ ui.R
@@ -3,38 +3,35 @@ library(shiny)
 # Based on code from:
 #   https://shiny.rstudio.com/articles/build.html
 
-# UI to display budget data for a city.
-shinyUI(pageWithSidebar(
+# Displays budget data for a city.
+shinyUI(fluidPage(
+  titlePanel("Budget Explorer"),
+  sidebarLayout(
+    # Controls to select the level of the budget charted.
+    sidebarPanel(
+      p(h4(em("Prototype only!"))),
+      p(h4(em("This is a work in progress"))),
+      selectInput("budgetLevel", "Level:",
+                  list("Service Area" = "service_area_code",
+                       "Bureau (not implemented)" = "bureau_code",
+                       "Accounting Object (not implemented)" = "object_code")),
+      selectInput("fiscalYear", "Fiscal Year:",
+                  list("2015-16" = "2015-16",
+                       "2014-15" = "2014-15",
+                       "2013-14" = "2013-14",
+                       "2012-13" = "2012-13",
+                       "2011-12" = "2011-12",
+                       "2010-11" = "2010-11",
+                       "2009-10" = "2009-10",
+                       "2008-09" = "2008-09",
+                       "2007-08" = "2007-08",
+                       "2006-07" = "2006-07",
+                       "All Years" = "All Years"))
+    ),
 
-  # Application title.
-  headerPanel("Budget Explorer"),
-
-  # Sidebar with controls to select the level of the budget
-  # to be included.
-  sidebarPanel(
-    p(h4(em("Prototype only!"))),
-    p(h4(em("This is a work in progress"))),
-    selectInput("budgetLevel", "Level:",
-                list("Service Area" = "service_area_code",
-                     "Bureau" = "bureau_code",
-                     "Accounting Object" = "object_code")),
-    selectInput("year", "Fiscal Year:",
-                list("2015-16" = "2015-16",
-                     "2014-15" = "2014-15",
-                     "2013-14" = "2013-14",
-                     "2012-13" = "2012-13",
-                     "2011-12" = "2011-12",
-                     "2010-11" = "2010-11",
-                     "2009-10" = "2009-10",
-                     "2008-09" = "2008-09",
-                     "2007-08" = "2007-08",
-                     "2006-07" = "2006-07",
-                     "All Years" = "All Years"))
-  ),
-
-  # Show the caption and plot of the requested variable.
-  mainPanel(
-    h3(textOutput("caption")),
-    plotOutput("budgetPlot")
+    # Shows the caption and plot of the requested variable.
+    mainPanel(
+      plotOutput("budgetPlot")
+    )
   )
 ))
